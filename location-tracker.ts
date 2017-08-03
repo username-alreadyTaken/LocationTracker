b9c2b950b9d74cc9bcfe6c45a73ba05cd3b950bf
Ajay Kumawat <ajaykumawat01@gmail.com>
2017-08-03 10:34:56 +0530

    Add files via upload

---
+++ location-tracker.ts
@@ -0,0 +1,88 @@
+import { Injectable, NgZone } from '@angular/core';
+import { BackgroundGeolocation, BackgroundGeolocationConfig } from '@ionic-native/background-geolocation';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+//import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+
+/*
+  Generated class for the LocationTrackerProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+    
+@Injectable()
+export class LocationTracker {
+ public watch: any;    
+  public lat: number = 0;
+  public lng: number = 0;
+ public BackgroundGeolocation = {};
+  constructor(public zone: NgZone, private backgroundGeolocation: BackgroundGeolocation, private geolocation: Geolocation) {
+    console.log('Hello LocationTrackerProvider Provider');
+   // const config: BackgroundGeolocationConfig = {};
+  }
+
+
+  startTracking() {
+ // Background Tracking
+ 
+  let config = {
+    desiredAccuracy: 0,
+    stationaryRadius: 20,
+    distanceFilter: 10, 
+    debug: true,
+    interval: 2000 
+  };
+ 
+  this.backgroundGeolocation.configure(config).subscribe((location) => {
+ 
+    console.log('BackgroundGeolocation:  ' + location.latitude + ',' + location.longitude);
+ 
+    // Run update inside of Angular's zone
+    this.zone.run(() => {
+      this.lat = location.latitude;
+      this.lng = location.longitude;
+    });
+ 
+  }, (err) => {
+ 
+    console.log(err);
+ 
+  });
+ 
+  // Turn ON the background-geolocation system.
+  this.backgroundGeolocation.start();
+ 
+ 
+  // Foreground Tracking
+ 
+let options = {
+  frequency: 3000, 
+  enableHighAccuracy: true
+};
+ 
+this.watch = this.geolocation.watchPosition(options).filter((p: any) => p.code === undefined).subscribe((position: Geoposition) => {
+ 
+  console.log(position);
+ 
+  // Run update inside of Angular's zone
+  this.zone.run(() => {
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;
+  });
+ 
+});
+ 
+}
+  
+ 
+  stopTracking() {
+ 
+ console.log('stopTracking');
+ 
+  this.backgroundGeolocation.finish();
+  this.watch.unsubscribe();
+  }
+
+}
